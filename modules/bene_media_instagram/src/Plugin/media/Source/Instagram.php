52b35d3a968d117d083284ce53c693bd11f0d1d2
Ben Di Maggio <ben@thinkshout.com>
2018-02-20 23:35:00 -0500

    Getting Twitter and Instagram working tolerably well. No config yet.

---
+++ modules/bene_media_instagram/src/Plugin/media/Source/Instagram.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\bene_media_instagram\Plugin\media\Source;
+
+use Drupal\bene_media\InputMatchInterface;
+use Drupal\bene_media\ValidationConstraintMatchTrait;
+use Drupal\media_entity_instagram\Plugin\Media\Source\Instagram as BaseInstagram;
+
+/**
+ * Input-matching version of the Instagram media type.
+ */
+class Instagram extends BaseInstagram implements InputMatchInterface {
+
+  use ValidationConstraintMatchTrait;
+
+}
