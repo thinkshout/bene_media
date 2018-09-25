83deaee4002a595e2b3f02662e53fdbd5ce40906
Gabe Carleton-Barnes <gabe@thinkshout.com>
2018-09-25 18:03:07 -0400

    Fix use path to prevent crashes on Pantheon.

--- modules/bene_media_instagram/src/Plugin/media/Source/Instagram.php
+++ modules/bene_media_instagram/src/Plugin/media/Source/Instagram.php
@@ -4,7 +4,7 @@ namespace Drupal\bene_media_instagram\Plugin\media\Source;
 
 use Drupal\bene_media\InputMatchInterface;
 use Drupal\bene_media\ValidationConstraintMatchTrait;
-use Drupal\media_entity_instagram\Plugin\Media\Source\Instagram as BaseInstagram;
+use Drupal\media_entity_instagram\Plugin\media\Source\Instagram as BaseInstagram;
 
 /**
  * Input-matching version of the Instagram media type.
